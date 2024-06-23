9fb17dedcb5aef84490bc5d672f05fa79da96c6d
naveenCoding12 <cnaveenkumar12@gmail.com>
2024-06-23 13:10:44 +0530

    "cycle sort"

--- DSA_CODING_PRAC/Sorting/Bubblesort/Bubble.java
+++ DSA_CODING_PRAC/Sorting/Bubblesort/Bubble.java
@@ -17,9 +17,9 @@ public class Bubble {
         // if array is sorted array
         boolean swap;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1 ; i++) {
             swap=false;
-            for (int j = 1; j < arr.length-i; j++) {
+            for (int j = 1; j < arr.length-i ; j++) {
                 if(arr[j]<arr[j-1]){
                     int temp=arr[j];
                     arr[j]=arr[j-1];
